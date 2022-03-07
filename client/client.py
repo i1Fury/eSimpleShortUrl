860491dbb1ae058a5fc6e04b3df8df4915fd8d09
Fury <ultimate.gaming.episodes@gmail.com>
2022-03-07 06:09:28 -0500

    Removed print statements

--- client/client.py
+++ client/client.py
@@ -20,8 +20,6 @@ def check_clip():
         resp = requests.post('/upload', files={'image': ('screenshot.png', bim, 'image/png')})
     else:
         if not (clip := paste()): return
-        print(clip)
-        print(re.match(r'(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})', clip))
         if re.match(r'(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})', clip):
             resp = requests.post(HOST + '/newurl?api=true', data={'url': clip})
         else:
