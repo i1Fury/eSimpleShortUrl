9f114e1a26ad882b9f0f08fbe982dcdbdf8b887b
Fury <ultimate.gaming.episodes@gmail.com>
2022-03-07 06:09:07 -0500

    Updated client for production use

--- client/client.py
+++ client/client.py
@@ -8,6 +8,8 @@ from win10toast import ToastNotifier
 
 toast = ToastNotifier()
 
+HOST = 'http://127.0.0.1.nip.io'
+
 
 def check_clip():
     im = ImageGrab.grabclipboard()
@@ -15,15 +17,15 @@ def check_clip():
         bim = io.BytesIO()
         im.save(bim, 'png')
         bim.seek(0)
-        resp = requests.post('http://127.0.0.1.nip.io/upload', files={'image': ('screenshot.png', bim, 'image/png')})
+        resp = requests.post('/upload', files={'image': ('screenshot.png', bim, 'image/png')})
     else:
         if not (clip := paste()): return
         print(clip)
         print(re.match(r'(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})', clip))
         if re.match(r'(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})', clip):
-            resp = requests.post('http://127.0.0.1.nip.io/newurl?api=true', data={'url': clip})
+            resp = requests.post(HOST + '/newurl?api=true', data={'url': clip})
         else:
-            resp = requests.post('http://127.0.0.1.nip.io/bin', data={'clip': clip})
+            resp = requests.post(HOST + '/bin', data={'clip': clip})
 
     if resp.status_code != 200: return  # Rate limited
     copy(resp.text)
